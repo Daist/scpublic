97f20c5f663ddcb4f7be3eee1535c9b96eeccc1a
Daist <rickstalker@list.ru>
2020-10-05 18:17:27 +0300

    Update BetaEditorLite.js

--- BetaEditorLite.js
+++ BetaEditorLite.js
@@ -67,7 +67,7 @@
     addGlobalStyle('.x-toolbar {padding: 0px 8px !important}'); //скукоживание высоты панелей
     addGlobalStyle('.l-comment-text {margin-top: 0px !important;}'); //скукоживание истории комментариев
     
-    addGlobalStyle('@media screen and (min-height: 1300px) {.l-segments__row.l-segments__row-selected {background-color: #bfdae0 !important;}}'); //с большим разрешением приходит большая ответственность
+    addGlobalStyle('@media screen and (min-height: 1200px) {.l-segments__row.l-segments__row-selected {background-color: #bfdae0 !important;}}'); //с большим разрешением приходит большая ответственность
     //addGlobalStyle('.l-segments__row.l-segments__row-selected {background-color: #CCFFCC !important;}'); //цвет фона текущего сегмента в редакторе; можно #BFDAE0
     //addGlobalStyle('.l-segments__row:nth-child(2n) {background-color: #F0F0F0 !important;}'); //цвет фона чётных сегментов в редакторе
     //addGlobalStyle('.l-segments__row:nth-child(odd) {background-color: #FFFFFF !important;}'); //цвет фона нечётных сегментов в редакторе
