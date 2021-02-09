eef617037faf3387f819433fc551c164c222a1bd
Daist <rickstalker@list.ru>
2021-02-10 00:31:53 +0300

    Update QAAutoExpandNumbers.js

--- QAAutoExpandNumbers.js
+++ QAAutoExpandNumbers.js
@@ -11,12 +11,11 @@
     head.appendChild(style);
 }
 
+    addGlobalStyle('.l-qa-check-report__segment-text [data-bind="text: sourceText"] {text-align: right}'); //выравнивание по правому краю текста оригинала
+    
     $(document).ready(function() { //When document has loaded
         //$('<style type="text/css">tr.l-corpr__trhover.l-qa-check-goSegment.clickable + tr {display: table-row!important;}</style>').appendTo($("head")); - после 15.03.2019 не пашет, поменяли код
         //$('<style type="text/css">tr.l-corpr__trhover.l-corpr__trhover_clickable.l-qa-check-goSegment + tr {display: table-row!important;}</style>').appendTo($("head")); - после 30.04.2019 не пашет, поменяли код
         $('<style type="text/css">tr.l-corpr__trhover.l-corpr__trhover_clickable.l-qa-check-report__go-segment + tr {display: table-row!important;}</style>').appendTo($("head"));
     });
-
-    addGlobalStyle('.l-qa-check-report__segment-text [data-bind="text: sourceText"] {text-align: right}'); //выравнивание по правому краю текста оригинала
-
 })();
