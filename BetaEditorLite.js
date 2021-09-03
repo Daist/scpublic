e0182023de2cc1b293bab76bedd6c14ad049c61f
Daist <rickstalker@list.ru>
2021-09-03 12:06:22 +0300

    Update BetaEditorLite.js

--- BetaEditorLite.js
+++ BetaEditorLite.js
@@ -13,11 +13,11 @@
 
     //padding
     //addGlobalStyle('.l-segments__cell.l-segments__cell-number, .l-segments__cell.l-segments__cell-editor.js-source-cell, .l-segments__cell.l-segments__cell-editor.js-target-cell, .l-segments__cell.l-segments__cell-info, .l-segments__cell__editor.source, .l-segments__cell__editor.target {padding: 0px 20px 0px 8px !important;}');
-	addGlobalStyle('.l-segments__cell.l-segments__cell_number, .l-segments__cell_editor, .l-segments__cell.l-segments__cell-info {padding: 0px 20px 0px 8px !important;}'); // поля ячеек в редакторе - после 03.09.2021
+    addGlobalStyle('.l-segments__cell.l-segments__cell_number, .l-segments__cell_editor, .l-segments__cell.l-segments__cell-info {padding: 0px 20px 0px 8px !important;}'); // поля ячеек в редакторе - после 03.09.2021
 
     //window height-based safety
     //addGlobalStyle('@media screen and (min-height: 850px) {.l-segments__cell__editor.source, .l-segments__cell__editor.target {padding: 0px 20px 0px 8px !important; min-height: 36px !important;}}');
-	addGlobalStyle('@media screen and (min-height: 960px) {.l-segments__number-value {min-height: 36px !important;}}'); //мин. высота строки при высоте окна >960px = 36px - после 03.09.2021
+    addGlobalStyle('@media screen and (min-height: 960px) {.l-segments__number-value {min-height: 36px !important;}}'); //мин. высота строки при высоте окна >960px = 36px - после 03.09.2021
 
     //addGlobalStyle('.x-segments .x-grid-item {font-size: 16px !important; line-height: 16px !important;}'); //font size, line interval; possible fonts - font-family: "Tahoma", "Times New Roman", Roboto, Helvetica, Arial, sans-serif !important;
 
@@ -35,13 +35,13 @@
     
     //addGlobalStyle('.l-segments__cell__preview.g-icon.g-icon_preview.l-segments__cell__preview_alone {display:none !important;}'); //hide irritating preview icon
     //addGlobalStyle('.l-segments__cell__preview.g-icon.g-icon_preview {display:none !important;}'); //hide irritating preview icon
-	addGlobalStyle('.l-segments__preview.g-icon.g-icon_preview {display:none !important;}'); //прячем глаз - после 03.09.2021
+    addGlobalStyle('.l-segments__preview.g-icon.g-icon_preview {display:none !important;}'); //прячем глаз - после 03.09.2021
 
     addGlobalStyle('.l-workflow-progress-panel__ls-wordcount {color:#000000 !important;}'); // stat text color
     
     //Make non-confirmed stand out vs confirmed
     //addGlobalStyle('.l-segments__confirm-btn.l-segments__confirmed {font: 10px/1 "smartcat-icons" !important; color: #A0F9A0 !important;}'); //my tick is small and green
-	addGlobalStyle('.l-segments__confirm-btn.l-segments__confirm-btn_done {font: 10px/1 "smartcat-icons" !important; color: #A0F9A0 !important;}'); //галка помельче; цвет галки светлее - после 03.09.2021
+    addGlobalStyle('.l-segments__confirm-btn.l-segments__confirm-btn_done {font: 10px/1 "smartcat-icons" !important; color: #A0F9A0 !important;}'); //галка помельче; цвет галки светлее - после 03.09.2021
     addGlobalStyle('.l-segments__confirm-btn {font-weight: bold !important; color: #000000 !important;}'); //bold unconf tick
 	
     //addGlobalStyle('.l-segments__cell__info__text-workflow {font-weight: bold !important; font: 15px/1 !important; color: #000000 !important;}'); //bold work name
@@ -52,9 +52,9 @@
     //addGlobalStyle('.g-icon.g-icon_join {width: 120px !important; background-color: #CCFFCC !important;}'); //segment merge is big and green
 
     //addGlobalStyle('.l-cat__number, .l-cat__source-text, .l-cat__match, .l-cat__target-text, .l-cat__cell.l-cat-text {padding-top: 0px !important; padding-bottom: 0px !important;}'); //CAT - TM & TB - #, Orig, %, Tran. Bad cat, no padding for you
-	addGlobalStyle('.l-cat__cell.l-cat__number, .l-cat__cell.l-cat__cell_text, .l-cat__cell.l-cat__match {padding: 0px !important;}'); //CAT - TM & TB - номера, оригинал, %, перевод, отступы сверху и снизу - после 03.09.2021
+    addGlobalStyle('.l-cat__cell.l-cat__number, .l-cat__cell.l-cat__cell_text, .l-cat__cell.l-cat__match {padding: 0px !important;}'); //CAT - TM & TB - номера, оригинал, %, перевод, отступы сверху и снизу - после 03.09.2021
     //addGlobalStyle('.l-cat-row {min-height: 20px !important}'); //CAT - TM & TB - line height
-	addGlobalStyle('.l-cat__row {min-height: 20px !important}'); //CAT - TM & TB - высота строки - после 03.09.2021
+    addGlobalStyle('.l-cat__row {min-height: 20px !important}'); //CAT - TM & TB - высота строки - после 03.09.2021
     addGlobalStyle('.l-cat__cell.l-cat__number {width: 24px !important}'); //CAT - TM & TB - # - fixed width
 
     addGlobalStyle('.l-search-filter__toggle-extend-filter {min-width: 75px !important}'); //fixed width, no filter button wiggle
@@ -70,14 +70,14 @@
 
     addGlobalStyle('.l-revisions-toolbar {padding: 0px !important;}'); //rev history
     //addGlobalStyle('.l-revisions__column.l-revisions__column-number, .l-revisions__column.l-revisions__column-text, .l-revisions__column.l-revisions__column-date, .l-revisions__column.l-revisions__column-stage, .l-revisions__column.l-revisions__column-user, .l-revisions__column.l-revisions__column-save, .l-comment__row.l-comment_new.l-comment_own {padding: 0px 12px !important; min-height: 20px !important}'); //rev history & comments
-	addGlobalStyle('.l-revisions__header, .l-revisions__column, .l-revisions__column.l-revisions__column_text {padding: 0px 6px !important; min-height: 20px !important}'); //строка истории - после 03.09.2021
+    addGlobalStyle('.l-revisions__header, .l-revisions__column, .l-revisions__column.l-revisions__column_text {padding: 0px 6px !important; min-height: 20px !important}'); //строка истории - после 03.09.2021
     addGlobalStyle('.l-revisions__scrollable-body {top: 46px !important;}'); //rev history
 
     addGlobalStyle('.l-errors__column.l-errors__column-number, .l-errors__column.l-errors__column-text.l-errors__column-text_with-checker, .l-errors__column.l-errors__column-checker {padding: 0px 12px !important; min-height: 20px !important}'); //errors
     addGlobalStyle('.l-errors__scrollable-body {top: 20px !important;}'); //errors
     
     //addGlobalStyle('.l-segments__cell__match-percentage.yellow {color: #c36e04 !important;}');//fuzzy match color
-	addGlobalStyle('.l-segments__match-percentage.l-segments__match-percentage_yellow {color: #c36e04 !important;}');//цвет неполных соответствий - после 03.09.2021
+    addGlobalStyle('.l-segments__match-percentage.l-segments__match-percentage_yellow {color: #c36e04 !important;}');//цвет неполных соответствий - после 03.09.2021
     addGlobalStyle('.l-segments__cell__info__text-source {font-size: 12px !important;}');//fuzzy match font size
     
     //addGlobalStyle('.x-column-header-inner, .x-grid-cell-inner {padding: 0px 12px !important;}'); //shrink my history
@@ -87,7 +87,7 @@
     //addGlobalStyle('.l-comment__row {font-weight: 400 !important;}'); //fix for the fucked up comments font
     
     //addGlobalStyle('@media screen and (min-height: 1200px) {.l-segments__row.l-segments__row-selected {background-color: #bfdae0 !important;}}'); //With great resolution comes great color
-	addGlobalStyle('@media screen and (min-height: 1200px) {.l-segments__row.l-segments__row_selected.js-active-row {background-color: #bfdae0 !important;}}'); //с большим разрешением приходит большая ответственность - после 03.09.2021
+    addGlobalStyle('@media screen and (min-height: 1200px) {.l-segments__row.l-segments__row_selected.js-active-row {background-color: #bfdae0 !important;}}'); //с большим разрешением приходит большая ответственность - после 03.09.2021
     //addGlobalStyle('@media screen and (min-height: 1200px) {.l-segments__table {font-size: 17px !important}}'); //With great resolution comes greater font size
 
 })();
