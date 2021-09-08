153913803a06a534fc45318f7a948d8c661db289
Daist <rickstalker@list.ru>
2021-09-08 17:22:14 +0300

    Update BetaEditorLite.js

--- BetaEditorLite.js
+++ BetaEditorLite.js
@@ -75,13 +75,13 @@
 
     addGlobalStyle('.l-errors__column.l-errors__column-number, .l-errors__column.l-errors__column-text.l-errors__column-text_with-checker, .l-errors__column.l-errors__column-checker {padding: 0px 12px !important; min-height: 20px !important}'); //errors
     addGlobalStyle('.l-errors__scrollable-body {top: 20px !important;}'); //errors
-	
-    addGlobalStyle('.l-notification__container{padding: 7px !important;}'); //всплывающее окно уведомлений
     
     //addGlobalStyle('.l-segments__cell__match-percentage.yellow {color: #c36e04 !important;}');//fuzzy match color
     addGlobalStyle('.l-segments__match-percentage.l-segments__match-percentage_yellow {color: #c36e04 !important;}');//цвет неполных соответствий - после 03.09.2021
     addGlobalStyle('.l-segments__cell__info__text-source {font-size: 12px !important;}');//fuzzy match font size
-    
+
+    addGlobalStyle('div.l-notification__container {padding: 7px !important}'); //всплывающее окно уведомлений	
+	
     //addGlobalStyle('.x-column-header-inner, .x-grid-cell-inner {padding: 0px 12px !important;}'); //shrink my history
     //addGlobalStyle('.x-tab {padding: 0px 15px !important}'); //and tab headers
     //addGlobalStyle('.x-toolbar {padding: 0px 8px !important}'); //and tabs
